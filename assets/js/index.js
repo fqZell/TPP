f7948b85717babc8e4fffddd84acd5364c4c9890
fqZell <mulkyway00@gmail.com>
2024-05-17 16:33:49 +0300

    add to banner continue news section

--- assets/js/index.js
+++ assets/js/index.js
@@ -26,6 +26,28 @@ const searchInput = () => {
     });
 }
 
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+  
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+    },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  
+    // And if we need scrollbar
+    scrollbar: {
+      el: '.swiper-scrollbar',
+    },
+});
+
 const init = () => {
     burgerMenu();
     searchInput()
